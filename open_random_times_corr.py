d2c4ed28764ec8795a07ca6f9ac0f9134ac87e08
Fernando Monteiro <fernandomonteiro@Fernandos-MacBook-Pro.local>
2020-07-08 17:32:46 -0400

    resolution code included

---
+++ open_random_times_corr.py
@@ -0,0 +1,161 @@
+import glob, os, h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.signal as sp
+import matplotlib.mlab as mlab
+from scipy.optimize import curve_fit
+import datetime as dt
+import matplotlib.dates
+from statsmodels.stats.proportion import proportion_confint
+import matplotlib.cm as cmx
+import matplotlib.colors as colors
+from scipy.special import erf
+from scipy.interpolate import UnivariateSpline
+
+a = np.load("random_times_corr_inloop2.npy", encoding='latin1', allow_pickle=True)
+
+def histogram(c, bins):
+    h, b = np.histogram(c, bins = bins)
+    bc = np.diff(b)/2. + b[:-1]
+    return [h, bc]
+
+def gauss(x, a, b, c):
+    g = c*np.exp(-0.5*((x-a)/b)**2)
+    return g
+
+def labview_time_to_datetime(lt):
+
+    ### Convert a labview timestamp (i.e. time since 1904) to a
+    ### more useful format (pytho datetime object)
+
+    ## first get number of seconds between Unix time and Labview's
+    ## arbitrary starting time
+    lab_time = dt.datetime(1904, 1, 1, 0, 0, 0)
+    nix_time = dt.datetime(1970, 1, 1, 0, 0, 0)
+    delta_seconds = (nix_time - lab_time).total_seconds()
+
+    if isinstance(lt, float):
+        lab_dt = dt.datetime.fromtimestamp(lt - delta_seconds)
+        return lab_dt
+
+    else:
+        Lab_dt = []
+        for i in lt:
+            lab_dt = dt.datetime.fromtimestamp(i - delta_seconds)
+            Lab_dt.append(lab_dt)
+        return Lab_dt
+
+def getdata(fname, gain_error=1.0):
+    ### Get bead data from a file.  Guesses whether it's a text file
+    ### or a HDF5 file by the file extension
+
+    _, fext = os.path.splitext( fname )
+    f = h5py.File(fname,'r')
+    dset = f['beads/data/pos_data']
+    dat = np.transpose(dset)
+    dat = dat / 3276.7 ## hard coded scaling from DAQ
+    attribs = dset.attrs
+    time = dset.attrs["Time"]
+
+    return dat, attribs, f, time
+
+def getcorr_time_seconds(a):
+    corr = []
+    T = []
+    for i in range(len(a)):
+        c = float(a[i][1])
+        corr.append(c)
+        time = getdata(a[i][0])[-1] + float(a[i][2])/1e4
+        T.append(time)
+    return [np.array(corr), np.array(T)]
+
+def getcorr_timedate(a):
+    corr, T = getcorr_time_seconds(a)
+    T = labview_time_to_datetime(T)
+    return [corr, T]
+
+def plot_histogram(a, Nblocks):
+    total = len(a)
+    N = int(total/Nblocks)
+    newcorr = []
+    newtime = []
+    i = 0
+    for k in range(Nblocks):
+        try:
+            Blocks = a[i*N:(k+1)*N]
+        except:
+            Blocks = a[i*N:-1]
+        i = i + 1
+        corr, time = getcorr_time_seconds(Blocks)
+        newcorr.append(corr)
+        newtime.append(time)
+
+    corr_each = []
+    corr_err_each = []
+    mean_time = []
+
+    plt.figure()
+    for m in newcorr:
+        print ("len", m)
+        h, bc = histogram(m, 13)
+        sigma = []
+        for j in h:
+            if j == 0:
+                sigma.append(1.)
+            else:
+                sigma.append(j ** 0.5)
+        popt, pcov = curve_fit(gauss, bc, h, sigma=sigma)
+        corr_each.append(np.abs(popt[1]))
+        corr_err_each.append(pcov[1][1]**0.5)
+
+        plt.plot(bc, h, ".")
+        energy = np.linspace(2. * np.min(bc), 2. * np.max(bc), 100)
+        plt.plot(energy, gauss(energy, *popt))
+
+    for n in newtime:
+        mean_time.append(np.mean(n))
+
+    mean_time = np.array(mean_time)
+
+    mean_time = labview_time_to_datetime(mean_time)
+
+    plt.figure()
+    plt.errorbar(mean_time, corr_each, yerr = corr_err_each, fmt = ".")
+
+    return []
+
+
+plot_histogram(a, 10)
+
+corr, T = getcorr_timedate(a)
+
+h, bc = histogram(corr, 50)
+
+sigma = []
+for j in h:
+    if j == 0:
+        sigma.append(1.)
+    else:
+        sigma.append(j**0.5)
+
+popt, pcov = curve_fit(gauss, bc, h, sigma = sigma)
+
+energy = np.linspace(2.*np.min(bc), 2.*np.max(bc), 100)
+
+plt.figure()
+plt.errorbar(bc, h, yerr = sigma, fmt = ".")
+plt.semilogy(energy, gauss(energy, *popt))
+plt.ylim(0.9, 1.2*np.max(h))
+
+print (popt)
+print (pcov)
+
+print ("sigma total = ", np.abs(popt[1]))
+print ("sigma total err= ", pcov[1][1]**0.5)
+
+plt.figure()
+plt.plot(T, corr, ".")
+
+plt.show()
+
+#time = labview_time_to_datetime(getdata(a[i][0])[-1] + float(a[i][2]) / 1e4)
