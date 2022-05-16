2587613d64080d7e649dbae2015409da69c86656
David Moore <david.c.moore@yale.edu>
2022-05-16 13:52:26 -0400

    updates with energy lost in sphere

--- steriles/calculate_sensitivity.py
+++ steriles/calculate_sensitivity.py
@@ -68,7 +68,7 @@ def calc_limit(t12, A, loading_frac, num_spheres, livetime, bkg_pdf, sig_pdf, m,
 
 
 
-iso_list = ['p_32',] #'p_32', 'be_7','ar_37', 'v_49','cr_51',"fe_55", 'ge_68', 'se_72']
+iso_list = ['p_32','s_35','y_90'] #'p_32', 'be_7','ar_37', 'v_49','cr_51',"fe_55", 'ge_68', 'se_72']
 ## list of parameters to use (loading frac, num spheres, livetime)
 #params_list = [[1e-2, 1, 10], 
 #               [1e-2, 1000, 365], ]
