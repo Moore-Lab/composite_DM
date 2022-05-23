e07a91af7e7bef225b9b8ac6bb66ad8f44ecff60
davemoore <dcmoore@gmail.com>
2022-05-23 14:13:26 -0400

    fix

--- steriles/sub_dsq_jobs.sh
+++ steriles/sub_dsq_jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sbatch dsq-h_3_jobs_35.0_1.0e+03-2022-05-19.sh
-sbatch dsq-h_3_nonzero_jobs_35.0_1.0e+03-2022-05-19.sh
-sbatch dsq-h_3_jobs_50.0_1.0e+03-2022-05-19.sh
-sbatch dsq-h_3_nonzero_jobs_50.0_1.0e+03-2022-05-19.sh
-sbatch dsq-h_3_jobs_35.0_5.0e+02-2022-05-19.sh
-sbatch dsq-h_3_nonzero_jobs_35.0_5.0e+02-2022-05-19.sh
-sbatch dsq-h_3_jobs_50.0_5.0e+02-2022-05-19.sh
-sbatch dsq-h_3_nonzero_jobs_50.0_5.0e+02-2022-05-19.sh
-sbatch dsq-h_3_jobs_100.0_1.0e+02-2022-05-19.sh
-sbatch dsq-h_3_nonzero_jobs_100.0_1.0e+02-2022-05-19.sh
+sbatch dsq-p_32_jobs_100.0_1.0e+05-2022-05-17.sh
+sbatch dsq-p_32_nonzero_jobs_100.0_1.0e+05-2022-05-17.sh
+sbatch dsq-h_3_jobs_25.0_1.0e+03-2022-05-17.sh
+sbatch dsq-h_3_nonzero_jobs_25.0_1.0e+03-2022-05-17.sh
+sbatch dsq-s_35_jobs_50.0_1.0e+04-2022-05-17.sh
+sbatch dsq-s_35_nonzero_jobs_50.0_1.0e+04-2022-05-17.sh
+sbatch dsq-pr_143_jobs_50.0_1.0e+05-2022-05-17.sh
+sbatch dsq-pr_143_nonzero_jobs_50.0_1.0e+05-2022-05-17.sh
+sbatch dsq-ru_106_jobs_150.0_1.0e+05-2022-05-17.sh
+sbatch dsq-ru_106_nonzero_jobs_150.0_1.0e+05-2022-05-17.sh
